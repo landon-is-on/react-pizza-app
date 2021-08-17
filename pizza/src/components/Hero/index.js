6f8563c55bcde52ca14be4c1c8035e03cef27095
Landon Smothers <landonsmothers24@gmail.com>
2021-08-17 14:54:29 -0400

    add pics

---
+++ pizza/src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Navbar';
+import {heroContainer} from './HeroElements';
+const Hero = () => {
+    return (
+        <heroContainer>
+            <Navbar/>
+        </heroContainer>
+    );
+};
+
+export default Hero;
