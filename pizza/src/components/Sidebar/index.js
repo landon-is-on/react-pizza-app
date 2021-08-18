60e279e5eac2728be63eee4705b8e52c925c5c09
Landon Smothers <landonsmothers24@gmail.com>
2021-08-18 01:03:26 -0400

    made sidebar

---
+++ pizza/src/components/Sidebar/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { SidebarContainer,Icon,CloseIcon,SidebarMenu,SidebarLink,
+    SidebarRoute,  SideBtnWrap,} from "./SidebarElements";
+
+const Sidebar = ({isOpen,toggle}) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon>
+                <CloseIcon>
+
+                </CloseIcon>
+            </Icon>
+            <SidebarMenu>
+                <SidebarLink to="/">Pizzas</SidebarLink>
+                <SidebarLink to="/">Desserts</SidebarLink>
+                <SidebarLink to="/">Full Menu</SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+               <SidebarRoute to="/">Order</SidebarRoute> 
+            </SideBtnWrap>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
