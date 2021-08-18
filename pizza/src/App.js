b7395c72b7fc66985af76672eed0051fc50ad4ce
Landon Smothers <landonsmothers24@gmail.com>
2021-08-18 01:39:42 -0400

    finished feature section

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -4,15 +4,16 @@ import { GlobalStyle } from './globalStyle';
 import Hero from './components/Hero';
 import Products from './components/Products';
 import {productData} from './components/Products/data';
+import Feature from './components/Feature';
 
 
 function App() {
   return (
     <Router>
     <GlobalStyle/>
-    
    <Hero/>
    <Products heading='Choose your favorite' data={productData}/>
+     <Feature/>
      </Router>
   );
 }
