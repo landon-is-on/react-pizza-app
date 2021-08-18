1ab4f2e2383bf39a3e1e2bc93c9673f742d91258
Landon Smothers <landonsmothers24@gmail.com>
2021-08-18 00:29:33 -0400

    finished hero

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './components/Navbar'
 import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from './globalStyle';
 import Hero from './components/Hero';
--- pizza/src/components/Hero/index.js
+++ pizza/src/components/Hero/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar';
-import {heroContainer} from './HeroElements';
+import {HeroContainer, HeroContent, HeroItems,HeroH1,HeroP,HeroBtn} from './HeroElements';
 const Hero = () => {
     return (
-        <heroContainer>
+        <HeroContainer>
             <Navbar/>
-        </heroContainer>
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Greatest Pizza Ever</HeroH1>
+                <HeroP>Ready in 60 seconds</HeroP>
+                <HeroBtn>Place Order</HeroBtn>
+                </HeroItems>
+            </HeroContent>
+        </HeroContainer>
     );
 };
 
